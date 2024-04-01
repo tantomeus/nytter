67708dca2df29c235dfa63c6925d29483ddb269e
tantomeus <carlomanid@gmail.com>
2024-04-01 16:03:04 +0300

    Add routes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const userRouter = require('./routes/user.js');
+const authRouter = require('./routes/auth.js');
 
 dotenv.config();
 
 const connect = () => {
     mongoose.connect(process.env.MONGO).then(() => console.log('succsesfully connected to mongo')).catch((err) => console.error(err));
 }
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(3000, () => {
     connect();
